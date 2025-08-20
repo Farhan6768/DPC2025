8b5ab4acd4bcdf35d4ba724d24bed58a3855cbfe
Farhan Samir Ibushe <farhanibushe@gmail.com>
2025-08-20 19:44:01 +0530

    day3.c
    Problem : Find the Leaders in an Array
    You are given an integer array arr of size n. An element is considered a leader if it is greater than all the elements to its right. Your task is to find all such leader elements in the array.
    
    Input :
    An integer array arr of size n.
    Example : 
    arr = [16, 17, 4, 3, 5, 2]
    
    Output :
    Return an array containing all the leader elements in the order in which they appear in the original array.
    Example:
    Leaders: [17, 5, 2]

---
+++ day3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void findLeaders(int arr[], int n) {
+    int leaders[n];  
+    int count = 0;
+
+    int max_from_right = arr[n - 1]; 
+    leaders[count++] = max_from_right; 
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] > max_from_right) {
+            max_from_right = arr[i];
+            leaders[count++] = arr[i];
+        }
+    }
+    printf("Leaders: [");
+    for (int i = count - 1; i >= 0; i--) {
+        printf("%d", leaders[i]);
+        if (i > 0) printf(", ");
+    }
+    printf("]\n");
+}
+
+int main() {
+    int arr[] = {16, 17, 4, 3, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    findLeaders(arr, n);
+
+    return 0;
+}
